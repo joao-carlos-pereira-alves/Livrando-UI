ce6c5b032ec22839a7bd2bc53f7e485a2d9bfb7e
jennysol <jennyssoliver@gmail.com>
2023-09-19 21:26:48 -0300

    feat: add navbar layout

--- our_library/app.vue
+++ our_library/app.vue
@@ -1,3 +1,13 @@
 <template>
-  <NuxtPage />
+  <q-layout view="lHh lpr lFf" container style="height: 400px" class="shadow-2 rounded-borders">
+    <Navbar />
+    <q-page-container>
+      <NuxtPage />
+    </q-page-container>
+  </q-layout>
 </template>
+
+<script setup>
+import Navbar from './layouts/Navbar.vue'
+
+</script>
