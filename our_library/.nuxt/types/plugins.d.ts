4911b653bc55309bda27c8c90227e834f54c9810
jennysol <jennyssoliver@gmail.com>
2023-10-15 16:36:00 -0300

    feat: add sweet alert plugin

--- our_library/.nuxt/types/plugins.d.ts
+++ our_library/.nuxt/types/plugins.d.ts
@@ -15,7 +15,8 @@ type NuxtAppInjections =
   InjectionType<typeof import("../../node_modules/@nuxt/devtools/dist/runtime/plugins/devtools.server").default> &
   InjectionType<typeof import("../../node_modules/@nuxt/devtools/dist/runtime/plugins/devtools.client").default> &
   InjectionType<typeof import("../../node_modules/nuxt-quasar-ui/dist/runtime/provide").default> &
-  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/chunk-reload.client").default>
+  InjectionType<typeof import("../../node_modules/nuxt/dist/app/plugins/chunk-reload.client").default> &
+  InjectionType<typeof import("../../plugins/sweetalert2").default>
 
 declare module '#app' {
   interface NuxtApp extends NuxtAppInjections { }
